b9eafd10f1767b6f55cccd8a3b2625b1cc93c590
gaokefei <260480378@qq.com>
2023-10-31 17:46:56 +0800

    fix: lint error

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,6 @@ const { sxzz } = require('@sxzz/eslint-config')
 module.exports = sxzz({
   rules: {
     'unused-imports/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'off',
   },
 })
